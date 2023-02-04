abe4a927d51e1eebf7aaf77dacb2d47fdc32d861
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-05 01:03:58 +0600

    concat string uppercase

--- Day-02/cToF.js
+++ Day-02/cToF.js
@@ -1,6 +1,5 @@
-
 /* ============================================================================
-write a javascript programe  to convert temperatures form celcius to fahrenheit 
+write a javascript programe to convert temperatures form celcius to fahrenheit 
 ===============================================================================
 */
 // var temp = prompt('Please Enter The Temperature: ');
@@ -8,3 +7,4 @@ var temp = 5
 var cToF = 9/5* temp + 32;
 // alert ( 'Fahrenheit: ' + cToF +  ' Degree');
 console.log(cToF);
+
