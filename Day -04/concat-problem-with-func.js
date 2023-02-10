c47ba552e4dfe64bb0d4c291b7bfb5339d78c259
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-11 02:33:02 +0600

    string problem

--- Day -04/concat-problem-with-func.js
+++ Day -04/concat-problem-with-func.js
@@ -2,9 +2,10 @@
 Make a constructor function. that takes name, age, and address. Inside the function define a method called display. If someone call the display method. It will return a string like this  -  "My name is {name}. I am {age} years old. I live in {address}”   
 */
 function myBio(name, age, address){
-    console.log('I am ' + name + ' I am ' + age + ' year old.' + ' I live in ' + address);
+    console.log('My Name is ' + name + ' I am ' + age + ' year old.' + ' I live in ' + address);
 }
-myBio( 'kawsar', 18, 'mymensingh');
+myBio( 'kawsar kabir.', 19, 'mymensingh');
+
 
 
 
